ea467c2448fdd35c94809c354b2736021925b3ef
JeanCasquete <Yinrodriguez71@gmail.com>
2023-05-10 08:49:44 -0500

    subida de todo los metodos crud y las evidencias

--- Talleres/Taller4/index.js
+++ Talleres/Taller4/index.js
@@ -1,39 +1,67 @@
 const express = require('express')
-const cors  =require('cors')
+const cors = require('cors')
 
 const PORT = 8000;
 
-const app =  express();
+const app = express();
 app.use(cors()).use(express.json())
-app.use('/public',  express.static(__dirname+'/public'))
+app.use('/public', express.static(__dirname + '/public'))
 
 
-let clientes =[
-    { id: 1, nombre: 'Juan', apellido: 'Pérez', identificacion: '123456789' },
-    { id: 2, nombre: 'María', apellido: 'González', identificacion: '987654321' }
-  
+let clientes = [
+  { id: 1, nombre: 'Juan', apellido: 'Pérez', identificacion: '123456789' },
+  { id: 2, nombre: 'María', apellido: 'González', identificacion: '987654321' }
 ]
 
 // GET  -> Consulta de información
-app.get('/clientes', (req, res)=>{
-    res.status(200).send(clientes);
+app.get('/clientes', (req, res) => {
+  res.status(200).send(clientes);
 })
+
 // GET INDIVIDUAL -> Consulta pero de un solo elemento
-app.get('cliente/:identificacion', (req,res)=>{
-    const {identificacion} =  req.params
-    const studentsFilter =  clientes.filter(p=> p.identificacion.toString() === identificacion.toString())
-    if (studentsFilter.length>0)
-    {
-        return res.status(200).send(studentsFilter[0])
-    }
-    res.status(403).send({
-        message:'No pudo ser encontrado el recurso'
-    })
-    
-
+app.get('/cliente/:identificacion', (req, res) => {
+  const { identificacion } = req.params
+  const studentsFilter = clientes.filter(p => p.identificacion.toString() === identificacion.toString())
+  if (studentsFilter.length > 0) {
+    return res.status(200).send(studentsFilter[0])
+  }
+  res.status(403).send({
+    message: 'No pudo ser encontrado el recurso'
+  })
 })
 
+// POST -> Insertar información
+app.post('/agregar-cliente', (req, res) => {
+  const { body } = req
+  clientes.push(body);
+  res.status(201).send({
+    message: 'Dato insertado correctamente',
+    response: body
+  })
+})
 
-app.listen(PORT, ()=>{
-    console.log(`Server running in http://localhost${PORT}`);
+// PATCH OR PUT   -> Actualizar información
+app.put('/actualizar-cliente/:identificacion', (req, res) => {
+  const { identificacion, nombre, apellido } = req.body;
+  let cliente = clientes.filter(p => p.identificacion === identificacion)[0] || {}
+  cliente.nombre = nombre;
+  cliente.apellido = apellido;
+
+  res.status(202).send({
+    message: 'Dato modificado correctamente',
+    response: cliente
+  })
+})
+
+// DELETE -> Eliminar algún elemento de la lista
+app.delete('/eliminar/:identificacion', (req, res) => {
+  const { identificacion } = req.params
+  clientes = clientes.filter(p => p.identificacion.toString() !== identificacion.toString())
+  res.status(200).send({
+    message: 'Se eliminó el elemento correctamente'
+  })
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running in http://localhost:${PORT}`);
 })
